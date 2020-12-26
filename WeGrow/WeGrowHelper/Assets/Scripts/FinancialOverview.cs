a45dfd5c7c3e6ead32fe82f5c5fcb4815832e33e
Jonathan <fuse.business@gmail.com>
2020-12-26 20:31:12 +0100

    * Add MainMenu scene * Add FinancialOverview.cs

---
+++ WeGrow/WeGrowHelper/Assets/Scripts/FinancialOverview.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinancialOverview : MonoBehaviour
+{
+    private decimal currentTotalMoney;
+    private decimal expectedMonthlyChange;
+
+    void Start()
+    {
+
+    }
+
+    private void FindExpectedMonthlyChange()
+    {
+
+    }
+}

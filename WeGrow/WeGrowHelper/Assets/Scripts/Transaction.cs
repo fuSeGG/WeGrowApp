211fcfb364ed6bfd88fbcc427e8cbe6c89b27b6e
Jonathan <fuse.business@gmail.com>
2020-12-26 21:14:28 +0100

    * Add Transaction

---
+++ WeGrow/WeGrowHelper/Assets/Scripts/Transaction.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transaction : MonoBehaviour
+{
+    public enum Frequency { Yearly, Monthly, Weekly, Daily, Hourly, OneTime }
+
+    public decimal MoneyAmount { get; set; }
+    public Frequency frequency { get; set; }
+}
